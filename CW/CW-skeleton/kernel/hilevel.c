9e12c3d11e50d66321edcf39d95b466fac46becf
aidan hood <ah16165@bristol.ac.uk>
2019-04-05 14:50:30 +0100

    typo

--- CW/CW-skeleton/kernel/hilevel.c
+++ CW/CW-skeleton/kernel/hilevel.c
@@ -213,7 +213,7 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
         i++;}
 
       // if no available pcb slot, don't fork
-      if (z = -1){
+      if (z == -1){
         break;
       }
 
